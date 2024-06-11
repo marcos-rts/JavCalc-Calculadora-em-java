a154e8e62582da6b6396dc9a2ed6af22eab5b5ad
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-11 12:16:33 -0300

    Base da calculadora

--- JavCalc/src/App.java
+++ JavCalc/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.Soma(5, 9);
+        System.out.println(calculadora.resultado);
     }
 }
---
+++ JavCalc/src/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora {
+
+    // Atributos da classe
+    float resultado;
+
+    void Soma(int n1, int n2){
+        resultado = n1+n2;
+    }
+
+    void Subtracao(int n1, int n2){
+        resultado = n1-n2;
+    }
+
+    void Multiplicacao(int n1, int n2){
+        resultado = n1*n2;
+    }
+
+    void divisao(int n1, int n2){
+        resultado = n1/n2;
+        
+    }
+
+    
+}
