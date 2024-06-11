ee45d5e2651c2ab51df24530417ff2cda3b5a953
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-11 17:11:23 -0300

    Adcionando escolha interativa via Scanner

--- JavCalc/src/App.java
+++ JavCalc/src/App.java
@@ -2,8 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Calculadora calc = new Calculadora();
 
-        calc.Soma(10, 5);
-        System.out.println(calc.getResultado()); // Deve imprimir o resultado da soma
-        System.out.println(calc.getMsgFinal()); // Deve imprimir a mensagem formatada
+        calc.Start();
+
     }
 }
--- JavCalc/src/Calculadora.java
+++ JavCalc/src/Calculadora.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class Calculadora {
+
+    Scanner scanner = new Scanner(System.in);
+
 
     // Atributos da classe
     private float resultado;
     private String operacao;
     private String msgFinal;
     private float sobra;
+    private int escolha;
+
+    void Start(){
+        System.out.println("Escolha a operação que deseja fazer:\n Adição - 1\n Subtração - 2\n Divisão - 3\n Multiplicação - 4\n Sair - 0" );
+        int escolha = scanner.nextInt();
+        Escolha(escolha);
+    }
+
+    /**
+     * @param escolha
+     */
+    void Escolha(int escolha){
+        System.out.print("Digite o primeiro numero: ");
+        int n1 = scanner.nextInt();
+        System.out.print("Digite o segundo numero: ");
+        int n2 = scanner.nextInt();
+        
+        
+        switch (escolha) {
+            case 1:
+                Soma(n1, n2);
+                break;
+
+            case 2:
+                Subtracao(n1, n2); 
+                break;
+            
+            case 3:
+                Divisao(n1, n2);
+                break;
+             
+            case 4:
+                Multiplicacao(n1, n2);
+                break;
+
+            default:
+                break;
+        }
+    }
 
     void Soma(int n1, int n2) {
         operacao = "soma";
@@ -82,5 +126,13 @@ public class Calculadora {
 
     public void setSobra(float sobra) {
         this.sobra = sobra;
+    }
+
+    public int getEscolha() {
+        return escolha;
+    }
+
+    private void setEscolha(int escolha) {
+        this.escolha = escolha;
     }
 }
