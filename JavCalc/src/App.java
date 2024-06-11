ca26224d2aa0e1a801f9a32264ccfe5fce26df5a
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-11 13:00:29 -0300

    adicionando mensagem final e visual

--- JavCalc/src/App.java
+++ JavCalc/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Calculadora calculadora = new Calculadora();
 
-        calculadora.Soma(5, 9);
-        System.out.println(calculadora.resultado);
+        calculadora.Divisao(37, 5);
+        System.out.println(calculadora.msgFinal);
     }
 }
--- JavCalc/src/Calculadora.java
+++ JavCalc/src/Calculadora.java
@@ -1,24 +1,48 @@
 public class Calculadora {
 
     // Atributos da classe
-    float resultado;
+    private float resultado;
+    private String operacao = "";
+    String msgFinal;
+    float sobra;
 
     void Soma(int n1, int n2){
+        operacao = "soma";
         resultado = n1+n2;
+        Resultado(resultado, n1, n2, operacao);
     }
 
     void Subtracao(int n1, int n2){
+        operacao = "subtracao";
         resultado = n1-n2;
+        Resultado(resultado, n1, n2, operacao);
     }
 
     void Multiplicacao(int n1, int n2){
+        operacao = "multiplicacao";
         resultado = n1*n2;
+        Resultado(resultado, n1, n2, operacao);
     }
 
-    void divisao(int n1, int n2){
+    void Divisao(int n1, int n2){
+        operacao = "divisao";
         resultado = n1/n2;
+        sobra = n1%n2;
+
+        Resultado(resultado, n1, n2, operacao);
         
     }
 
+    private void Resultado(float resultado, int n1, int n2, String operacao){
+        if (operacao == "soma") {
+            msgFinal = n1 + " + " + n2 + "\n---\n= " + resultado;
+        }if (operacao == "subtracao") {
+            msgFinal = n1 + " - " + n2 + "\n---\n= " + resultado;
+        }if (operacao == "multiplicacao") {
+            msgFinal = n1 + " x " + n2 + "\n---\n= " + resultado;
+        }if (operacao == "divisao") {
+            msgFinal = n1 + " / " + n2 + "\n---\n= " + resultado + "\nSObras: " + sobra;
+        }
+    }
     
 }
