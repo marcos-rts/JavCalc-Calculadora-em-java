c84f5bd5454912354dd834e3e2c161b7f313cb81
Marcos Alexandre Reis Torquato dos Santos <50344569+marcos-rts@users.noreply.github.com>
2024-06-12 16:27:10 -0300

    remoção de funções que não deram certo

--- JavCalc/src/App.java
+++ JavCalc/src/App.java
@@ -2,7 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Calculadora calc = new Calculadora();
 
-        calc.Start();
 
     }
 }
--- JavCalc/src/Calculadora.java
+++ JavCalc/src/Calculadora.java
@@ -10,45 +10,6 @@ public class Calculadora {
     private String operacao;
     private String msgFinal;
     private float sobra;
-    private int escolha;
-
-    void Start(){
-        System.out.println("Escolha a operação que deseja fazer:\n Adição - 1\n Subtração - 2\n Divisão - 3\n Multiplicação - 4\n Sair - 0" );
-        int escolha = scanner.nextInt();
-        Escolha(escolha);
-    }
-
-    /**
-     * @param escolha
-     */
-    void Escolha(int escolha){
-        System.out.print("Digite o primeiro numero: ");
-        int n1 = scanner.nextInt();
-        System.out.print("Digite o segundo numero: ");
-        int n2 = scanner.nextInt();
-        
-        
-        switch (escolha) {
-            case 1:
-                Soma(n1, n2);
-                break;
-
-            case 2:
-                Subtracao(n1, n2); 
-                break;
-            
-            case 3:
-                Divisao(n1, n2);
-                break;
-             
-            case 4:
-                Multiplicacao(n1, n2);
-                break;
-
-            default:
-                break;
-        }
-    }
 
     void Soma(int n1, int n2) {
         operacao = "soma";
@@ -128,11 +89,4 @@ public class Calculadora {
         this.sobra = sobra;
     }
 
-    public int getEscolha() {
-        return escolha;
-    }
-
-    private void setEscolha(int escolha) {
-        this.escolha = escolha;
-    }
 }
